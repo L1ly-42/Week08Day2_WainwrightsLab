5d693269638284196afa464d1628971d8bb8832e
Leila Peltier <l_peltier@outlook.com>
2024-03-19 14:07:21 +0000

    created getAllWainwrights() function

--- wainwrights_start_code/app.js
+++ wainwrights_start_code/app.js
@@ -1,1 +1,6 @@
 
+const getAllWainwrights = async ()=>{
+    const response = await fetch(`https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json`)
+    const jsonData = await response.json();
+    return jsonData;
+};
