597504e60276d6f40d4247ac26fe57caaeeff456
Leila Peltier <l_peltier@outlook.com>
2024-03-19 15:53:18 +0000

    assigned getAllWainwrights() return data to global variable

--- wainwrights_start_code/app.js
+++ wainwrights_start_code/app.js
@@ -1,6 +1,17 @@
+let allWainwrights;
+const wainwrightList = document.querySelector('#wainwrights-list');
 
 const getAllWainwrights = async ()=>{
     const response = await fetch(`https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json`)
     const jsonData = await response.json();
-    return jsonData;
+    // console.log(jsonData);
+    allWainwrights = jsonData;
+    // console.log(allWainwrights[0]); 
 };
+
+
+
+getAllWainwrights();
+
+
+
